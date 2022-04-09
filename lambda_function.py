c18390a75d589c64a00f44b844d3b48f0499a002
Pavithra2712 <pavithra.p@edstem.com>
2022-04-09 06:39:57 +0000

    commit changes

--- lambda_function.py
+++ lambda_function.py
@@ -14,12 +14,11 @@ def lambda_handler(event, context):
     
     try:
         temp_file = "/tmp/hello_local.txt"
-    
-     s3.Bucket(BUCKET_NAME).download_file(KEY, temp_file)
-      file = open(temp_file, "r")
-       return {
-        'statusCode': 200,
-        'body': file.read
+        s3.Bucket(BUCKET_NAME).download_file(KEY, temp_file)
+        file = open(temp_file, "r")
+        return {
+         'statusCode': 200,
+         'body': file.read
         }
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == "404":
